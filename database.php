2d2885423d95659d84c0bfe518eee281b3147bcb
Jan Jeric Tablico <jerictablico@gmail.com>
2022-02-02 15:56:45 +0800

    Edited the credentials for connecting to MYSQL database

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "Jerbear#1799";
+    $servername = "127.0.0.1";
+    $username = "azure";
+    $password = "6#vWHD_$";
     $db="books";
-    $port = 3306;
+    $port = 53720;
     $conn = mysqli_connect($servername, $username, $password,$db, $port);
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
