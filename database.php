8b3e354cbebc4ef22db3cf9457a38919b1673384
Jan Jeric Tablico <jerictablico@gmail.com>
2022-02-02 15:21:59 +0800

    update in database.php

--- database.php
+++ database.php
@@ -3,7 +3,8 @@
     $username = "root";
     $password = "Jerbear#1799";
     $db="books";
-    $conn = mysqli_connect($servername, $username, $password,$db);
+    $port = 3306;
+    $conn = mysqli_connect($servername, $username, $password,$db, $port);
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
