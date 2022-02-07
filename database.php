d535826c6c2ddb7e49b1c0171279773a3a775b6f
Jan Jeric Tablico <jerictablico@gmail.com>
2022-02-07 11:37:28 +0800

    Modified database.php

--- database.php
+++ database.php
@@ -9,11 +9,9 @@ parse_str($finalString, $result);
 $conn = new mysqli($result['Data Source'], $result['User Id'], $result['Password']);
 
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-mysqli_close($conn);
 
 // $servername = "127.0.0.1";
 // $username = "azure";
