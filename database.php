889239d82ad8f23e55d5178eb82d965469440308
Jan Jeric Tablico <jerictablico@gmail.com>
2022-02-07 11:22:20 +0800

    Modified the database.php file

--- database.php
+++ database.php
@@ -1,11 +1,27 @@
 <?php
-    $servername = "127.0.0.1";
-    $username = "azure";
-    $password = "6#vWHD_$";
-    $db="books";
-    $port = 53815;
-    $conn = mysqli_connect($servername, $username, $password,$db, $port);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-?>
+$AzureConnString = $_SERVER['MYSQLCONNSTR_localdb'];
+$AzureConnStringPieces = explode(";", $AzureConnString);
+
+$finalString = implode("&",$AzureConnStringPieces);
+
+parse_str($finalString, $result);
+
+echo $result['Database'];
+
+$conn = mysqli_connect($result['Data Source'], $result['User Id'], $result['Password'], $result['Database']);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_close($conn);
+
+// $servername = "127.0.0.1";
+// $username = "azure";
+// $password = "6#vWHD_$";
+// $db = "books";
+// $port = 53815;
+// $conn = mysqli_connect($servername, $username, $password, $db, $port);
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
