ea4a64fb8db914ca73c732454d02b372a6feb473
Jan Jeric Tablico <jerictablico@gmail.com>
2022-02-11 13:30:07 +0800

    Put commented lines above the code block

--- database.php
+++ database.php
@@ -1,4 +1,9 @@
 <?php
+// $servername = "127.0.0.1";
+// $username = "azure";
+// $password = "6#vWHD_$";
+// $db = "books";
+// $conn = mysqli_connect($servername, $username, $password, $db);
 $connArray = explode(';', $_SERVER['MYSQLCONNSTR_localdb']);
 $connItems = [];
 
@@ -9,12 +14,7 @@ foreach ($connArray as $pair) {
 list ($host, $port) = explode(':', $connItems['Data Source']);
 
 $conn = mysqli_connect($host, $connItems['User Id'], $connItems['Password'], "books", $port);
-// $servername = "127.0.0.1";
-// $username = "azure";
-// $password = "6#vWHD_$";
-// $db = "books";
-// $port = 53815;
-// $conn = mysqli_connect($servername, $username, $password, $db, $port);
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
